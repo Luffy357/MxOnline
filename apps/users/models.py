476fdd6066ac8876ff5d3a5c723b73aef7495d4c
Luffy357 <34535690+Luffy357@users.noreply.github.com>
2018-10-17 20:26:37 +0800

    connect database

---
+++ apps/users/models.py
@@ -0,0 +1,47 @@
+# -*- coding:utf8 -*-
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(verbose_name=u'昵称', max_length=50, default='')
+    birday = models.DateField(verbose_name=u'生日', null=True, blank=True)
+    gender = models.CharField(choices=(("male", u'男生'), ("female", u'女生')), default='', max_length=10)
+    address = models.CharField(max_length=100, default=u"")
+    mobile = models.ImageField(upload_to="image/%Y/%m", default=u'image/default.png')
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u'验证码')
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱')
+    send_type = models.CharField(choices=(("register", u'注册'), ("forget", u"找回")), max_length=10)
+    send_time = models.DateField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name=u'轮播图')
+    url = models.URLField(max_length=200, verbose_name=u'访问地址')
+    index = models.IntegerField(default=100, verbose_name=u'顺序')
+    add_time = models.DateField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
+
