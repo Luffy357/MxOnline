2a45f9d946b835a9281d233e2d7f8573090a3913
Luffy357 <34535690+Luffy357@users.noreply.github.com>
2018-10-23 21:18:10 +0800

    实现分页

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,5 +1,7 @@
+# -*- coding:utf8 -*-
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from organization.models import CourseOrg, CityDict
@@ -12,8 +14,18 @@ class OrgListView(View):
 
         org_nums = all_org.count()
 
+        # 实现分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_org, 2, request=request)
+
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            'all_org': all_org,
+            'all_org': orgs,
             'all_city': all_city,
             'org_nums': org_nums,
         })
