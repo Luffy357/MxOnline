8805bb28a5b1044f058a822712c2eba35a7776e4
Luffy357 <34535690+Luffy357@users.noreply.github.com>
2018-10-25 23:35:45 +0800

    完成机构模块，bug刷新页面已收藏变收藏

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -2,15 +2,21 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
 
-from .views import OrgListView, AddUserAskView, OrgHomeView
+from .views import OrgListView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 urlpatterns = [
     # 机构列表页
     url(r'^list/$', OrgListView.as_view(), name='org_list'),
     # 课程咨询
     url(r'^add_ask/$', AddUserAskView.as_view(), name="add_ask"),
+    # 机构首页
+    url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home"),
+    url(r'^org_course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name="org_course"),
+    url(r'^org_desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name="org_desc"),
+    url(r'^org_teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name="org_teacher"),
 
-    url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name="org_home")
+    # 机构收藏
+    url(r'^add_fav/$', AddFavView.as_view(), name="add_fav"),
 
 
 
