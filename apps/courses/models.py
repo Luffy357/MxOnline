fae9234076967d2f2eaee2e1b7029525b4ed7e50
Luffy357 <34535690+Luffy357@users.noreply.github.com>
2018-10-28 21:50:05 +0800

    完成课程详情页，播放页

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from datetime import datetime
 
 from django.db import models
-from organization.models import CourseOrg
+from organization.models import CourseOrg, Teacher
 # Create your models here.
 
 
@@ -12,6 +12,7 @@ class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name=u'课程名称')
     desc = models.TextField(max_length=500, verbose_name=u'课程描述')
     degree = models.CharField(choices=(('cj', u'初级'), ('zj', u'中级'), ('gj', u'高级')), max_length=10)
+    teacher = models.ForeignKey(Teacher, verbose_name=u'授课讲师', null=True, blank=True)
     learn_times = models.IntegerField(default=0, verbose_name=u'学习时长')
     students = models.IntegerField(default=0, verbose_name=u'学习人数')
     fav_nums = models.IntegerField(default=0, verbose_name=u'收藏人数')
@@ -19,6 +20,8 @@ class Course(models.Model):
     click_nums = models.IntegerField(default=0, verbose_name=u'点击数')
     category = models.CharField(max_length=20, verbose_name=u'课程类别', default=u'基础课程')
     tag = models.CharField(max_length=20, default='', verbose_name=u'标签')
+    youneed_know = models.TextField(max_length=500, verbose_name=u'课程须知')
+    teacher_tell = models.TextField(max_length=500, verbose_name=u'老师告诉你')
     add_time = models.DateField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
@@ -32,13 +35,13 @@ class Course(models.Model):
     def get_zj_nums(self):
         return self.lesson_set.count()
 
-    # 获取章节数
-    def get_course_nums(self):
-        return self.lesson_set.all()
-
     # 获取学习用户
     def get_learn_users(self):
         return self.usercourse_set.all()[:5]
+
+    # 获取课程所有章节
+    def get_course_lesson(self):
+        return self.lesson_set.all()
 
 
 class Lesson(models.Model):
@@ -53,10 +56,15 @@ class Lesson(models.Model):
     def __unicode__(self):
         return self.name
 
+    def get_lesson_video(self):
+        # 获取章节视频
+        return self.video_set.all()
+
 
 class Video(models.Model):
     lesson = models.ForeignKey(Lesson, verbose_name=u'视频')
     name = models.CharField(max_length=100, verbose_name=u'视频名')
+    times = models.IntegerField(default=0, verbose_name=u'视频时长')
     url = models.CharField(max_length=200, verbose_name=u'访问地址', default='')
     add_time = models.DateField(default=datetime.now, verbose_name=u'添加时间')
 
@@ -70,7 +78,7 @@ class Video(models.Model):
 
 class CourseResource(models.Model):
     course = models.ForeignKey(Course, verbose_name=u'课程')
-    name = models.CharField(max_length=100, verbose_name=u'视频名')
+    name = models.CharField(max_length=100, verbose_name=u'资源名')
     download = models.FileField(upload_to='course/resource/%Y/%m', verbose_name=u'资源文件', max_length=100)
     add_time = models.DateField(default=datetime.now, verbose_name=u'添加时间')
 
--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -2,15 +2,21 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import url
 
-from .views import CourseListView, CourseDetailView, CourseInfoView
+from .views import CourseListView, CourseDetailView, CourseInfoView, CourseCommentView, AddCommentView, VideoPlayView
 urlpatterns = [
     # 课程列表页
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
 
     # 课程详情列表
-    url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
+    url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name="course_detail"),
 
     # 课程章节信息
-    url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_video'),
+    url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
+    # 课程评论
+    url(r'^comment/(?P<course_id>\d+)/$', CourseCommentView.as_view(), name='course_comment'),
+    # 提交评论
+    url(r'^add_comment/$', AddCommentView.as_view(), name='add_comment'),
+    # 视频播放
+    url(r'^video/(?P<video_id>\d+)/$', VideoPlayView.as_view(), name='video_play'),
 ]
 
